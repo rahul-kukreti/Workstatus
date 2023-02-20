ab9978f94ac2e7a9397c411954a79bfdc7f350a2
rahul-kukreti <rahul.kukreti@mail.vinove.com>
2023-02-20 17:22:19 +0530

    forgot commit

---
+++ src/test/java/com/End2End/PagesObjects/Forgotpassword_Objectpage.java
@@ -0,0 +1,95 @@
+package com.End2End.PagesObjects;
+
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+import com.End2End.Test_Regression.BaseClass;
+
+public class Forgotpassword_Objectpage extends BaseClass {
+
+	By fp_link = By.xpath("//a[@href='/auth/forgot-password']");
+	By email_field = By.xpath("//input[@formcontrolname='email']");
+	By search_mail = By.xpath("//input[@id='login']");
+	By reset_btn = By.xpath("//a[contains(@href,'https://app.newstaging.workstatus.io/auth/reset-password')]");
+	By button = By.xpath("//button[contains(text(),'OK')]");
+	By password = By.xpath("//input[@formcontrolname='password']");
+	By passwordConfirm = By.xpath("//input[@formcontrolname='passwordConfirm']");
+	By reset_pass = By.xpath("//span[contains(text(),'Reset Password')]");
+
+	public void click_fpLink(WebDriver driver) {
+		commFunc.Explicitywait(driver, fp_link);
+		commFunc.Click(driver, fp_link);
+	}
+
+	public void validate_fpLink(WebDriver driver) {
+		commFunc.Explicitywait(driver, By.xpath("//h1[text()=' Recover Your Password ']"));
+		if (driver.findElements(By.xpath("//h1[text()=' Recover Your Password ']")).size() != 0) {
+			System.out.println("Reset page displayed!");
+		} else {
+			System.err.println("Reset page is not displayed!");
+		}
+	}
+
+	public void email_field(WebDriver driver) {
+		driver.findElement(email_field).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+		commFunc.sendKeyswithEnter(driver, email_field, (conf.getemail()));
+	}
+
+	public void wrong_email(WebDriver driver) {
+		commFunc.sendKeyswithEnter(driver, email_field, (conf.getWrong_email()));
+	}
+
+	public void validate_invalidEmail(WebDriver driver) {
+		commFunc.Explicitywait(driver, By.xpath("//h2[text()='Email not found.']"));
+		if (driver.findElements(By.xpath("//h2[text()='Email not found.']")).size() != 0) {
+			System.out.println("Email not found.!");
+			commFunc.Click(driver, button);
+		} else {
+			System.err.println("Wrong email popup is not displayed!");
+		}
+	}
+	
+	public void validate_ValidEmail(WebDriver driver) {
+		commFunc.Explicitywait(driver, By.xpath("//div[contains(text(),'Reset your password')]"));
+		if (driver.findElements(By.xpath("//div[contains(text(),'Reset your password')]")).size() != 0) {
+			System.out.println("Email sent to user..!");
+		} else {
+			System.err.println("Wrong email popup is not displayed!");
+		}
+	}
+
+	public void getWindow(WebDriver driver) throws InterruptedException {
+
+		driver.navigate().to("https://yopmail.com/en/");
+		commFunc.sendKeyswithEnter(driver, search_mail, (conf.getemail()));
+		driver.switchTo().frame("ifmail");
+		commFunc.Explicitywait(driver, reset_btn);
+		commFunc.actionsClick(driver, reset_btn);
+		ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+		driver.switchTo().window(tabs.get(1));
+		commFunc.Explicitywait(driver, By.xpath("//input[@formcontrolname='password']"));
+		commFunc.sendKeyswithEnter(driver, password,(conf.getpassword()));
+		commFunc.sendKeyswithEnter(driver, passwordConfirm,(conf.getConfirm_passowrd()));
+	}
+	
+	public void click_resetbtn(WebDriver driver) {
+		commFunc.Explicitywait(driver, reset_pass);
+		commFunc.Click(driver, reset_pass);
+	}
+
+	
+	public void validate_resetpass(WebDriver driver) {
+		commFunc.Explicitywait(driver, By.xpath("//h2[text()='Password updated successfully.']"));
+		if(driver.findElements(By.xpath("//h2[text()='Password updated successfully.']")).size()!=0) {
+			System.out.println("Reset password validate succcessfully!");
+			commFunc.Click(driver, button);
+		}
+		else {
+			System.err.println("Email not found..!");
+		}
+	}
+	 
+}
---
+++ src/test/java/com/End2End/Test_Regression/Forgotpassword.java
@@ -0,0 +1,46 @@
+package com.End2End.Test_Regression;
+
+import java.util.ArrayList;
+
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+public class Forgotpassword extends BaseClass {
+
+	@Test(testName = "Click forgot password link", priority = 15, enabled = true)
+	public void click_support_feat() {
+		logger = rep.startTest("Forgot password scenarios");
+		System.out.println("//For clicking on forgot password link..!!");
+		fp.click_fpLink(driver);
+		fp.validate_fpLink(driver);
+		logger.log(LogStatus.INFO, "forgot password link successfully!");
+
+	}
+
+	@Test(testName = "Click invalid email in forgot password ", priority = 16, enabled = true)
+	public void validate_wrongemail() {
+		System.out.println("//For validating wrong email..!!");
+		fp.wrong_email(driver);
+		fp.validate_invalidEmail(driver);
+		logger.log(LogStatus.INFO, "Inavlid email for forgot password validated successfully!");
+
+	}
+
+	@Test(testName = "Click valid email in forgot password ", priority = 17, enabled = true)
+	public void validate_validemail() {
+		System.out.println("//For validating valid email..!!");
+		fp.email_field(driver);
+		fp.validate_ValidEmail(driver);
+		logger.log(LogStatus.INFO, "Valid email for forgot password validated successfully!");
+	}
+
+	@Test(testName = "Click valid email in forgot password ", priority = 18, enabled = true)
+	public void forgot_password() throws InterruptedException {
+		System.out.println("//For validating forgot password..!!");
+		fp.getWindow(driver);
+		fp.click_resetbtn(driver);
+		fp.validate_resetpass(driver);
+		logger.log(LogStatus.INFO, "Forgot password validated successfully!");
+	}
+}
